681bc82177af2764a44d1eba9663f145a805767c
Rohan Appinventiv <rohan.gupta@gmail.com>
2022-09-06 13:45:25 +0530

    Deleting unwanted files

--- app/src/main/java/com/rohan/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/rohan/jetpackcompose/MainActivity.kt
@@ -2,35 +2,29 @@ package com.rohan.jetpackcompose
 
 import android.os.Bundle
 import android.os.Parcelable
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.mapSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.rohan.jetpackcompose.ui.composables.AdaptiveRowItem
-import com.rohan.jetpackcompose.ui.composables.MyCustomBox
-import com.rohan.jetpackcompose.ui.theme.JetPackComposeTheme
-import com.rohan.jetpackcompose.ui.theme.Shapes
 import kotlinx.parcelize.Parcelize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AdaptiveRowItem()
             /*HelloText(counter.value){
                 counter.value++
             }*/
